5242d0cf4c6580b54f6be7a49dd6cf2e9d7aaca4
Janis Giolo <janis.giolo@students.fhnw.ch>
2026-05-28 18:41:32 +0200

    Delete views directory

--- views/__init__.py
+++
@@ -1,5 +0,0 @@
-from views.etf_view import etf_seite
-from views.detail_view import detail_seite
-from views.vergleich_view import vergleich_seite
-
-__all__ = ["etf_seite", "detail_seite", "vergleich_seite"]
